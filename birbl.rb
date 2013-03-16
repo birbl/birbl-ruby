77b34c6e9516f952306d1d1aace2f4fc76ba52b9
Aaron Craig <aaron@birbl.com>
2013-03-16 18:14:09 +0000

    aaron: update for git

--- birbl.rb
+++ birbl.rb
@@ -5,6 +5,13 @@ Birbl::Client.new('development')
 Birbl::Client.instance.dev_url = 'http://localhost:8080'
 Birbl::Client.instance.use_sandbox = true
 
+#p = Birbl::Partner.new(
+#  :name         => 'test',
+#  :description  => 'This & that',
+#  :email        => 'foo@example.com'
+#)
+#p.save
+
 p = Birbl::Partner.find(1)
-p.users<< {:id => 1}
+puts p.to_yaml
 p.save
